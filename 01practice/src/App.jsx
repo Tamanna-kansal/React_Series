a8f4c4e293a0ddc08a7c6c1815bf42809ac1fe8e
Tamanna kansal <tamannakansal118@gmail.com>
2024-08-30 23:59:53 +0530

    Practice of Rendering

---
+++ 01practice/src/App.jsx
@@ -0,0 +1,10 @@
+import Button from "./components/button";
+
+export default function App() {
+  return (
+    <>
+      <h1>This is my first file rendering buttons</h1>
+      <Button />
+    </>
+  )
+}
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,11 @@
 import Chai from './chai';
 function App() {
+  const username = "Tamanna Kansal"
   return (
     <>
       <Chai />
       <h1>Chai aur code</h1>
+      <h2>{username}</h2>
     </>
   )
 }
