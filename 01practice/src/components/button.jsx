a71d4b8978db4e7c62e68aae2a214c5a006ecaaa
Tamanna kansal <tamannakansal118@gmail.com>
2024-08-31 00:44:37 +0530

    practiing

--- 01practice/src/components/button.jsx
+++ 01practice/src/components/button.jsx
@@ -1,5 +1,17 @@
 export default function Button() {
+    //creating object
+    const user = {
+        name: 'Hedy Lamarr',
+        imageUrl: 'https://i.imgs.jpg',
+        imagesize: 90,
+    };
+
     return (
-        <button> I 'm a Button</button>
+        <>
+            <h1>username :{user.name}</h1>
+            <h2>imaheUrl :{user.imageUrl}</h2>
+            <h3>imagesize :{user.imagesize}</h3>
+            <button> I 'm a Button</button>
+        </>
     )
 }
